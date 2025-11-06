1a826ebf89ce30fdbf7c24dae4c646b1872b53f6
Raji2084 <kamaraj.raji@gmail.com>
2025-11-06 14:54:35 +0000

    ArrayListPractice

--- src/test/java/Practice/ArrayListPractice.java
+++ src/test/java/Practice/ArrayListPractice.java
@@ -1,6 +1,22 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class ArrayListPractice {
 
+ public static void main(String[] args) {
+
+     ArrayList<Integer> numbers = new ArrayList<Integer>();
+     numbers.add(100);
+     numbers.add(200);
+     numbers.add(300);
+     numbers.add(400);
+     numbers.remove(0);
+
+     for(int i=0; i<numbers.size(); i++){
+         System.out.println(numbers.get(i));
+     }
+
+ }
 
 }
--- src/test/java/Practice/ArrayPractice.java
+++ src/test/java/Practice/ArrayPractice.java
@@ -5,16 +5,16 @@ package Practice;
 public class ArrayPractice
 {
     /* static variables belong to the class and allows main() to access them directly
-    without needing to create an object of TwoArraysPractice. */
+    without needing to create an object of ArraysPractice. */
 
     ///Declare and initialize arrays
-    static int[] Array1 = {10,20,30,40,50}; //static variable
-    static int[] Array2 = {05,15,25,35,45}; //static variable
+    //static int[] Array1 = {10,20,30,40,50}; //static variable
+    //static int[] Array2 = {05,15,25,35,45}; //static variable
 
     public static void main(String[] args)
     {
-        //int[] Array1 = {10,20,30,40,50}; //non static variable
-        //int[] Array2 = {05,15,25,35,45}; //non static variable
+        int[] Array1 = {10,20,30,40,50}; //non static variable
+        int[] Array2 = {5,15,25,35,45}; //non static variable
 
         System.out.println("Array1 list");
 
