50bea2e40e99bdbe74f796449a473d44586ddddb
Raji2084 <kamaraj.raji@gmail.com>
2025-11-21 14:52:57 +0000

    ParentchildPractice

--- src/test/java/Practice/ParentChildLocatorsPractice.java
+++ src/test/java/Practice/ParentChildLocatorsPractice.java
@@ -3,12 +3,13 @@ package Practice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 public class ParentChildLocatorsPractice {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -19,12 +20,21 @@ public class ParentChildLocatorsPractice {
 
         // xpath - //form/input
         // css - form input
-        //Assert class from testng frameowrk
+        //Assert class from testing framework
         // Testng and junit are frameworks
 
         //<input type="text" placeholder="Username" id="inputUsername" value="" fdprocessedid="i6ds8d">
-        //driver.findElement(By.cssSelector());
+        driver.findElement(By.cssSelector("#container div:nth-child(2) form input:nth-child(2)")).sendKeys("Raji123");
+        //<input type="password" placeholder="Password" name="inputPassword" value="" fdprocessedid="yom2pr">
+        driver.findElement(By.cssSelector("#container div:nth-child(2) form input:nth-child(3)")).sendKeys("rahulshettyacademy");
+        //<button class="submit signInBtn" type="submit" fdprocessedid="k55wvk">Sign In</button>
+        driver.findElement(By.className("signInBtn")).click();
 
+        Thread.sleep(1000);
+
+       String SuccessMessage = driver.findElement(By.cssSelector("div p")).getText();
+       System.out.println(SuccessMessage);
+       Assert.assertEquals(SuccessMessage, "You are successfully logged in.");
 
     }
 
