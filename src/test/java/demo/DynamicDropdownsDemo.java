75e31b80e0f0ed726c0269b1784fdfe0aa1280ea
Gunashekar <chgunareddy@gmail.com>
2025-11-25 21:40:38 +0530

    dynamic dropdown

--- src/test/java/demo/DynamicDropdownsDemo.java
+++ src/test/java/demo/DynamicDropdownsDemo.java
@@ -9,6 +9,8 @@ import org.testng.Assert;
 import java.time.Duration;
 import java.util.List;
 
+import static javax.swing.text.html.CSS.getAttribute;
+
 public class DynamicDropdownsDemo {
 
     public static void main(String[] args) throws InterruptedException {
@@ -39,6 +41,48 @@ public class DynamicDropdownsDemo {
         }
 
         driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+
+        // 1. click senior citizen check box
+        // 2. how many check boxes are there in webpage
+        // 3. is checkbox enabled
+        // 4. is dropdown displayed and enabled
+
+         WebElement seniorCitizenCheckbox = driver.findElement(By.cssSelector("input[name*='SeniorCitizenDiscount']"));
+
+        List<WebElement> checkboxesList = driver.findElements(By.cssSelector("input[type='checkbox']"));
+
+        System.out.println(checkboxesList.size());
+
+        for (WebElement checkbox:checkboxesList){
+
+            System.out.println(checkbox.isDisplayed());
+
+        }
+
+        if (seniorCitizenCheckbox.isDisplayed() && seniorCitizenCheckbox.isEnabled()) {
+
+            seniorCitizenCheckbox.click();
+        }
+
+        System.out.println(seniorCitizenCheckbox.isSelected());
+
+        Assert.assertTrue(seniorCitizenCheckbox.isSelected());
+
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+        WebElement returnDate = driver.findElement(By.cssSelector("#Div1"));
+
+       String attributeValue =  returnDate.getAttribute("style").split("opacity:")[1].trim().replace(";","");  // display: block; opacity: 0.5; //0.5
+
+        float opacityValue = Float.parseFloat(attributeValue);
+
+        if (opacityValue == 0.5){
+
+            System.out.println("Return Date is Disabled");
+        }
+        else {
+            System.out.println("Return Date is Enabled");
+        }
 
         driver.quit();
 
