acf68e2df0c11bd8b186ecc2d3262de39ce1b1ef
Raji2084 <kamaraj.raji@gmail.com>
2025-12-08 22:07:12 +0000

    DoWhile and Nested Loop practice

---
+++ src/test/java/Practice/DoWhileLoopPractice.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class DoWhileLoopPractice {
+
+    public static void main(String[] args) {
+
+        //Print numbers from 1 to 10
+
+        int i = 1;
+
+        //While Loop
+
+       /* while(i<=10) {
+
+            System.out.println(i);
+            i++;
+
+        } */
+
+        //DoWhile Loop
+
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+    }
+
+}
---
+++ src/test/java/Practice/NestedLoopPractice.java
@@ -0,0 +1,77 @@
+package Practice;
+
+public class NestedLoopPractice {
+
+    public static void main(String[] args) {
+
+        /* 1. output for below code : 1
+                                      1 2
+                                      1 2 3
+                                      1 2 3 4 */
+
+        /* for(int i=1;i<=4;i++){
+
+            for(int j=1;j<=i;j++){
+
+                System.out.print(j);
+                System.out.print("\t");
+
+            }
+            System.out.print("\n");
+        } */
+
+        /* 2. output for below code : 1
+                                      2 3
+                                      4 5 6
+                                      7 8 9 10 */
+        /* int k = 1;
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(k);
+                System.out.print("\t");
+                k++;
+
+            }
+            System.out.print("\n");
+        } */
+
+        /* 3. output for below code : 10 9 8 7
+                                      6 5 4
+                                      3 2
+                                      1 */
+       /* int k = 10;
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 4; j >= i; j--) {
+
+                System.out.print(k);
+                System.out.print("\t");
+                k--;
+            }
+            System.out.print("\n");
+        } */
+
+
+        /* 4. output for below code : * * * *
+                                      * * *
+                                      * *
+                                      *           */
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 4; j >= i; j--) {
+
+                System.out.print("*");
+                System.out.print("\t");
+            }
+            System.out.print("\n");
+        }
+
+
+
+    }
+}
