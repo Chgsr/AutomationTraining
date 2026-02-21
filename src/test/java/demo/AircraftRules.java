a7bc65c35cf22021430a27c069c2f9d96f42d235
Gunashekar <chgunareddy@gmail.com>
2026-02-21 20:26:56 +0530

    Abstract

---
+++ src/test/java/demo/AircraftRules.java
@@ -0,0 +1,16 @@
+package demo;
+
+public abstract   class AircraftRules  {
+
+
+    public void name(){
+
+        System.out.println("Name Aircraft");
+    }
+    public void safety(){
+        System.out.println("Safety");
+    }
+    public abstract void color();
+
+
+}
